11f6b7ec7c4f7225dabb228a42b5245225029073
Dinesh45-creator <dineshgunala45@gmail.com>
2025-12-20 15:08:37 +0530

    Add Task1.java to demonstrate array usage

---
+++ Module1_Array/Task1.java
@@ -0,0 +1,17 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Try programiz.pro");
+        
+        int[] n={10,20,30,40};
+        System.out.println(n[0]);//zeroth index_> 1st element of array
+        
+        System.out.println(n[1]);
+        System.out.println(n[2]);
+        System.out.println(n[3]);
+        System.out.println(n.length+"total elements in array");
+        
+        
+        
+        
+    }
+}
