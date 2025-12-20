bd0c3ac7664915a3d38bb3cb07beb08c4ab0f740
Dinesh45-creator <dineshgunala45@gmail.com>
2025-12-20 15:34:41 +0530

    Refactor array handling and output in Task1.java
    Refactored array initialization and improved output formatting.

--- Module1_Array/Task1.java
+++ Module1_Array/Task1.java
@@ -1,17 +1,26 @@
 class Main {
     public static void main(String[] args) {
-        System.out.println("Try programiz.pro");
-        
-        int[] n={10,20,30,40};
-        System.out.println(n[0]);//zeroth index_> 1st element of array
-        
+        int[]n={10,20,30,40};
+        System.out.println(n[0]);
         System.out.println(n[1]);
         System.out.println(n[2]);
         System.out.println(n[3]);
-        System.out.println(n.length+"total elements in array");
+        System.out.println(n.length);
         
+        for(int i=0; i<n.length; i++){
+            System.out.println("at index "+ i + " is " + n[i]);
+        }
         
+        int[] n1= new int[] {1,2,3,4};
         
+        for(int i=0; i<n1.length; i++){
+            System.out.println("at index "+ i + " is " + n1[i]);
+        }
+        
+        String[] names =new String[] {"jeet","raj","ram"};
+        for(int i=0; i<names.length; i++){
+            System.out.println("at index "+ i + " is " + names[i]);
+        }
         
     }
 }
