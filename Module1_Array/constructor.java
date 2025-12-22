d3efafd9b75fa47c7f232713cac29155ff05d78e
Dinesh45-creator <dineshgunala45@gmail.com>
2025-12-22 12:02:27 +0530

    Add student class with constructor and main method

--- Module1_Array/constructor.java
+++ Module1_Array/constructor.java
@@ -1,1 +1,20 @@
+class student {
+    String name;
+    int age;
+
+
+    student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        student b = new student("ram", 27);
+        System.out.println(b.name);
+        System.out.println(b.age);
+    }
+}
 
